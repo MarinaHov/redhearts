e27c697bb2c90e0aae793352b07e87212f737f01
Marina Hovjecki <marinahovjecki@Marinas-MacBook-Pro.local>
2017-05-14 19:51:52 +0200

    Initial Commit

---
+++ SweetHearts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SweetHearts
+//
+//  Created by Marina Hovjecki on 5/14/17.
+//  Copyright © 2017 Marina Hovjecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
